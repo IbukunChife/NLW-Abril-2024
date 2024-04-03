f4dd17f6ecd6e59cc6dd6b8ede17f8fc0ea92684
Ibukun Didier <adjitchedidier1@gmail.com>
2024-04-03 18:43:15 -0400

    feat:implement  checkInRepository

---
+++ src/models/repository/check_ins_repository.py
@@ -0,0 +1,24 @@
+from src.models.settings.connection import db_connection_handler
+from src.models.entities.check_ins import CheckIns
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class CheckInRepository:
+    def insert_check_in(self, attendee_id) -> str:
+
+        with db_connection_handler as database:
+            try:
+                check_in = (
+                    CheckIns(AttendeeId=attendee_id)
+                )
+                database.session.add(check_in)
+                database.session.commit()
+                return attendee_id
+
+            except IntegrityError:
+                raise Exception('Check In já realizado!')
+
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
