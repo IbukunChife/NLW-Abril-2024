76d0c4fa99c20d969fdbd55a22453cf1eca21ccf
Ibukun Didier <adjitchedidier1@gmail.com>
2024-04-03 22:01:27 -0400

    feat: Implementing get event route

--- src/main/routes/event_routes.py
+++ src/main/routes/event_routes.py
@@ -1,11 +1,24 @@
 from flask import Blueprint, jsonify, request
 from src.http_types.http_request import HttpRequest
+from src.data.event_handler import EventHandler
 
 event_route_bp = Blueprint("event_route", __name__)
 
 
-@event_route_bp.route("/events", methods=["GET", "POST"])
+@event_route_bp.route("/events", methods=["POST"])
 def create_event():
-
+    event_handler = EventHandler()
     http_request = HttpRequest(body=request.json)
-    return jsonify({"ola": "mundo"}), 200
+
+    http_response = event_handler.register(http_request)
+    return jsonify(http_response.body), http_response.status_code
+
+
+@event_route_bp.route("/events/<event_id>", methods=["GET"])
+def get_event(event_id):
+    event_handler = EventHandler()
+    http_request = HttpRequest(param={"event_id": event_id})
+
+    http_response = event_handler.find_by_id(http_request)
+    return jsonify(http_response.body), http_response.status_code
+
